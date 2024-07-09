7171cd54556faab99912d05ad63a9bc1163bb489
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-09 23:29:06 +0530

    doing changes  in  common.sh

--- common.sh
+++ common.sh
@@ -8,27 +8,27 @@ Nodejs(){
 
   dnf module disable nodejs -y
   print  installing nodejs 20
-  dnf module enable nodejs:20 -y
+  dnf module enable nodejs:20 -y &>>$LOG_FILE
 
   dnf install nodejs -y
   print adding user roboshop
-  useradd roboshop
+  useradd roboshop &>>$LOG_FILE
 
-  rm -rf /app
+  rm -rf /app &>>$LOG_FILE
   print removing directory
 
-  mkdir /app
+  mkdir /app &>>$LOG_FILE
   print creating directory
 
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
   print  downloading app content
 
   cd /app
-  unzip /tmp/${component}.zip
+  unzip /tmp/${component}.zip &>>$LOG_FILE
   print extracting app content
 
-  cd /app
-  npm install
+  cd /app &>>$LOG_FILE
+  npm install &>>$LOG_FILE
   print download nodejs dependencies
 
   systemctl daemon-reload
@@ -37,13 +37,13 @@ Nodejs(){
 }
 print(){
   echo &>>LOG_FILE
-  "###########$*###########" &>>LOG_FILE
+  "###########$*###########" &>>$LOG_FILE
   echo $*
 }
 demo(){
-  echo z value is ${z} &>>LOG_FILE
+  echo z value is ${z} &>>$LOG_FILE
 #  echo z value is $z &>>LOG_FILE
 #  echo z value is $((z)) &>>LOG_FILE
 }
 demo
-demo
+
