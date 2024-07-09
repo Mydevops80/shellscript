d03d119802fb56f8b3215362110c4a72db4804b9
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-09 20:37:56 +0530

    doing changes  in common.sh

--- common.sh
+++ common.sh
@@ -4,6 +4,7 @@ Nodejs(){
 
   cp ${component}.service /etc/systemd/system/${component}.service
   print  copying service file
+
   dnf module disable nodejs -y
   print  installing nodejs 20
   dnf module enable nodejs:20 -y
@@ -11,13 +12,24 @@ Nodejs(){
   dnf install nodejs -y
   print adding user roboshop
   useradd roboshop
+
   rm -rf /app
+  print removing directory
+
   mkdir /app
+  print creating directory
+
   curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip
+  print  downloading app content
+
   cd /app
   unzip /tmp/${component}.zip
+  print extracting app content
+
   cd /app
   npm install
+  pinrt download nodejs dependencies
+
   systemctl daemon-reload
   systemctl enable ${component}
   systemctl restart ${component}
