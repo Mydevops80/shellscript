32c80d6b555b3c998315a7d737f1bfc1d3aad215
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-10 10:06:02 +0530

    changes in common.sh

--- common.sh
+++ common.sh
@@ -50,10 +50,10 @@ print(){
 }
 stat(){
   if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success in stat \e[0m" &>>$LOG_FILE
+    echo -e "\e[32m Success \e[0m" &>>$LOG_FILE
   else
-      echo -e "\e[31m Failure IN stat \e[0m" &>>$LOG_FILE
-      echo "refer log file "
+      echo -e "\e[31m Failure \e[0m" &>>$LOG_FILE
+      echo  -e"\e[31m refer log file \e[0m  /tmp/roboshop"
       exit
   fi
 }
