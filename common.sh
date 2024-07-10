dfbe989bd60ba25912c2d6f0e04f4884fc752962
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-10 10:17:28 +0530

    changes in common.sh

--- common.sh
+++ common.sh
@@ -49,7 +49,7 @@ print(){
   echo $* &>>$LOG_FILE
 }
 stat(){
-  if [ $? -eq 0 ] ; then
+  if [ $1 -eq 0 ]; then
     echo -e "\e[32m Success \e[0m" &>>$LOG_FILE
   else
       echo -e "\e[31m Failure \e[0m" &>>$LOG_FILE
