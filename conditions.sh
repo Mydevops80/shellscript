3460b468c2d011b2a592fe9df0e2ccd516ebd340
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-10 09:55:14 +0530

    changes in common.sh

--- conditions.sh
+++ conditions.sh
@@ -1,21 +1,22 @@
 LOG_FILE=/tmp/temp.log
-rm -f LOG_FILE
+rm -f $LOG_FILE
 if [ $? -eq 0 ] ; then
   echo -e "\e[32m Success in IF \e[0m"
 else
-    echo -e "\e[31m Failure \e[0m" &>>LOG_FILE
+    echo -e "\e[31m Failure \e[0m" &>>$LOG_FILE
 fi
 stat(){
   if [ $? -eq 0 ] ; then
-    echo -e "\e[32m Success in stat \e[0m" &>>LOG_FILE
+    echo -e "\e[32m Success in stat \e[0m" &>>$LOG_FILE
   else
-      echo -e "\e[31m Failure IN stat \e[0m" &>>LOG_FILE
+      echo -e "\e[31m Failure IN stat \e[0m" &>>$LOG_FILE
+      echo "refer log file "
       exit
   fi
 }
 user(){
   if [ $? -ne 0 ] ; then
-   useradd pavani  &>>LOG_FILE
+   useradd pavani  &>>$LOG_FILE
     fi
 }
 stat
