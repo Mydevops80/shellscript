c9d58f65a7963d20e4d765366c95b09c237188db
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-10 09:35:04 +0530

    changes in conditions.sh

--- conditions.sh
+++ conditions.sh
@@ -3,3 +3,12 @@ if [ $? -eq 0 ] ; then
 else
     echo -e "\e[31m Failure \e[0m"
 fi
+stat(){
+  if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success \e[0m"
+  else
+      echo -e "\e[31m Failure \e[0m"
+      exit
+  fi
+}
+stat
