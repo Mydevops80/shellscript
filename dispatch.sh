b15857faf7649a114052f37d15b6c6b144729cba
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-11 16:23:38 +0530

    changes in dispatch.sh

--- dispatch.sh
+++ dispatch.sh
@@ -4,11 +4,11 @@ app_path=/app
 
 APP_PREQ
 print installing golang
-dnf install golang -y
+dnf install golang -y &>>$LOG_FILE
 stat $?
 
 print running go build
-go mod init dispatch
-go get
-go build
+go mod init dispatch &>>$LOG_FILE
+go get &>>$LOG_FILE
+go build &>>$LOG_FILE
 SYSTEMD_SETUP
