139adec4f504895a99942e7a967c692761b28eb7
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-11 17:28:39 +0530

    changes rabbit.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,10 @@
 source common.sh
 component=rabbitmq-server
-schema_setup=rabbitmq
+
+print schema setup
+SCHEMA_SETUP rabbitmq
+stat $?
+
 print adding user to rabbitmq
 rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
