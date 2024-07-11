bc69dfbe2307396ddedcc3bd8ce55129d893ad02
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-11 10:07:42 +0530

    changes in mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -2,20 +2,20 @@ source common.sh
 component=mongo
 
 print  copy mongodb repo file
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 stat $?
 
 print installing mongodb
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$LOG_FILE
 stat $?
 
 print update mongod.conf file
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$LOG_FILE
 stat $?
 
 print starting mongod
-systemctl enable mongod
-systemctl start mongod
+systemctl enable mongod &>>$LOG_FILE
+systemctl start mongod  &>>$LOG_FILE
 #update the config file
-systemctl restart mongod
+systemctl restart mongod  &>>$LOG_FILE
 stat $?
