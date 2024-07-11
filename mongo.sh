c424850f1b6a260ef7e1bd8f46aefa005dcd95a9
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-11 10:00:33 +0530

    changes in mongo.sh

--- mongo.sh
+++ mongo.sh
@@ -3,19 +3,19 @@ component=mongo.
 
 print  copy mongodb repo file
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-stat$?
+stat $?
 
 print installing mongodb
 dnf install mongodb-org -y
-stat$?
+stat $?
 
 print update mongod.conf file
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-stat$?
+stat $?
 
 print starting mongod
 systemctl enable mongod
 systemctl start mongod
 #update the config file
 systemctl restart mongod
-stat$?
+stat $?
