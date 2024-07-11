9f0c63d29a53958dc0477cd4ab7bdabf95f63580
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-11 17:55:38 +0530

    changes user.sh

--- user.sh
+++ user.sh
@@ -1,9 +1,13 @@
+#source common.sh
+#component=user
+#app_path=/app
+#print copying service file  &>>$LOG_FILE
+#cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
+#stat $?
+#
+#Nodejs
 source common.sh
 component=user
 app_path=/app
-print copying service file  &>>$LOG_FILE
-cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-stat $?
 
-Nodejs
-
+NODEJS
