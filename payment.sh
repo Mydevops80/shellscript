49d7149b1c94adfe8777953e655393f2133dc382
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-11 16:10:22 +0530

    changes in payment.sh

--- payment.sh
+++ payment.sh
@@ -2,11 +2,12 @@ source common.sh
 component=payment
 app_path=/app
 
+
+APP_PREQ
+
 print installing Payment
 dnf install python3 gcc python3-devel -y &>>$LOG_FILE
 stat $?
 
-
-APP_PREQ
 SYSTEMD_SETUP
 stat $?
