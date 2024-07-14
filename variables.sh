663e0a92274aa56fd52403faa805840f130c0fd0
PavaniSurisetty <pavsurisetty@gmail.com>
2024-07-14 19:54:28 +0530

    adding variables.sh

--- variables.sh
+++ variables.sh
@@ -1,6 +1,8 @@
 name=Devops
 faculty=Raghu
+myname=Pavani Surisetty
 echo welcome to $name training
 echo $faculty is your trainer
+echo my name is  $myname
 #echo z value is $1
 #echo environment variable z=${z}
